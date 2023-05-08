a745e3f9462e7b4b5c9d64ec48908da3a42ceb04
harim <harim9504@gmail.com>
2023-05-08 11:33:47 +0900

    Revert ".gitignore 변경"
    This reverts commit 255dfd93d38c9a6bae32b4f03d0d349f7080725e.

--- src/main/java/com/example/justpost/domain/utils/FileUtil.java
+++ src/main/java/com/example/justpost/domain/utils/FileUtil.java
@@ -20,10 +20,10 @@ import java.util.List;
 public class FileUtil {
 
     public static final String POST_FILE_PATH = "src/main/resources/post/";
-    public static final String AFTER_POST_FILE_PATH = "src/main/resources/after-post/";
+    public static final String AFTER_POST_FILE_PATH = "src/main/resources/afterPost/";
     public static final String MERGE_FILE_PATH = "src/main/resources/merge/";
     public static final String POST_TEMPLATE_FILE_PATH = "src/main/resources/templates/post/";
-    public static final String AFTER_POST_TEMPLATE_FILE_PATH = "src/main/resources/templates/after-post/";
+    public static final String AFTER_POST_TEMPLATE_FILE_PATH = "src/main/resources/templates/afterPost/";
     public static final String MERGED_EXCEL_FILE_NAME = "mergedExcelFile.xlsx";
 
 
